7133b81fa465d25bff6f9ecb802b3126be571cc4
Aler97 <radicevicsasa836@gmail.com>
2021-11-28 16:08:23 +0100

    error handling

--- js/salaries.js
+++ js/salaries.js
@@ -4,51 +4,54 @@ const list = document.querySelector('.salaries');
 let listArr = [];
 
 async function getSalaries() {
-    const request = await fetch('https://data.montgomerycountymd.gov/resource/48wg-fkab.json');
-    const data = await request.json();
+    try {
+        const request = await fetch('https://data.montgomerycountymd.gov/resource/48wg-fkab.json');
+        const data = await request.json();
 
-    let li;
-    list.innerHTML = '';
-    data.forEach(element => {
-        li = document.createElement('li');
-        li.innerText = `${element['position_title']}: ${element['average_of_base_salary']}`;
-        list.appendChild(li);
-        listArr.push(li.innerText);
-    });
+        let li;
+        list.innerHTML = '';
+        data.forEach(element => {
+            li = document.createElement('li');
+            li.innerText = `${element['position_title']}: ${element['average_of_base_salary']}`;
+            list.appendChild(li);
+            listArr.push(li.innerText);
+        });
 
 
-    btn.addEventListener('click', () => {
-        if (inputSal.value != '') {
-            list.innerHTML = '';
-            listArr.forEach(salarie => {
-                if (salarie.toLowerCase().includes(inputSal.value)) {
+        btn.addEventListener('click', () => {
+            if (inputSal.value != '') {
+                list.innerHTML = '';
+                listArr.forEach(salarie => {
+                    if (salarie.toLowerCase().includes(inputSal.value)) {
+                        li = document.createElement('li');
+                        li.innerText = `${salarie}`;
+                        list.appendChild(li);
+
+                    }
+                });
+            }
+
+
+
+            if (list.innerHTML === '') {
+                list.innerHTML = `<b>Sorry. No information about that job.</b>`;
+            };
+        });
+
+        inputSal.addEventListener('input', () => {
+            if (inputSal.value == '') {
+                list.innerHTML = '';
+                data.forEach(element => {
                     li = document.createElement('li');
-                    li.innerText = `${salarie}`;
+                    li.innerText = `${element['position_title']}: ${element['average_of_base_salary']}`;
                     list.appendChild(li);
 
-                }
-            });
-        }
-
-
-
-        if (list.innerHTML === '') {
-            list.innerHTML = `<b>Sorry. No information about that job.</b>`;
-        };
-    });
-
-    inputSal.addEventListener('input', () => {
-        if (inputSal.value == '') {
-            list.innerHTML = '';
-            data.forEach(element => {
-                li = document.createElement('li');
-                li.innerText = `${element['position_title']}: ${element['average_of_base_salary']}`;
-                list.appendChild(li);
-
-            });
-        }
-    })
-
+                });
+            }
+        })
+    } catch {
+        list.innerHTML = `<b>Error occured. Please reload the page</b>`;
+    }
 }
 
 getSalaries();
